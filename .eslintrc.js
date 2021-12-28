c6a9f1ed59d92e3d66ac6c25dc94c85df0a1d5fc
吕道锋 <574818075@qq.com>
2021-12-28 18:32:06 +0800

    测试eslint3

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,12 @@ module.exports = {
     browser: true,
     es6: true,
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
 };
